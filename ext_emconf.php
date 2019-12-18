8c5ae3e9e389c225478a9670fa4d799db2d652b2
NITSAN <sanjay@nitsan.in>
2019-12-18 18:17:53 +0530

    [TODO] Corrected description of extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Google Map Plugin',
-    'description' => 'This extension brings integratio nof powerful features of Google map like find-places, find-direction between locations etc. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/google-map-by-nitsan-pro/',
+    'description' => 'This extension can easily integrate of powerful features of Google map like find-places, find-direction between locations etc. Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/google-map-by-nitsan-pro/',
     'category' => 'plugin',
     'author' => 'T3: Milan Rathod, QA: Siddharth Sheth',
     'author_email' => 'sanjay@nitsan.in',
