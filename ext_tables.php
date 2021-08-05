fb6ef9bdcca280956046a770fbf8c2b74f430010
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-05 18:03:15 +0530

    [TODO] Code improvements

--- ext_tables.php
+++ ext_tables.php
@@ -5,5 +5,5 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3_MODE') || die('Access denied.');
 
 ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_google_map/Configuration/TSconfig/ContentElementWizard.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_google_map/Configuration/TSconfig/ContentElementWizard.tsconfig">'
 );
