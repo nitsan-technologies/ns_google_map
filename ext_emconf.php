bd53b977278cdefae97afde827ff5c6eca9c1e70
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 15:13:32 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_google_map'] = [
 	'title' => '[NITSAN] Google Map Plugin',
-	'description' => 'This extension can easily integrate of powerful features of Google map like find-places, find-direction between locations etc. Live-Demo: https://demo.t3terminal.com//t3t-extensions/google-map You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-google-map-typo3-extension-pro',
+	'description' => 'This extension can easily integrate of powerful features of Google map like find-places, find-direction between locations etc. Live-Demo: https://demo.t3planet.com//t3t-extensions/google-map You can download PRO version for more-features & free-support at https://t3planet.com/typo3-google-map-typo3-extension-pro',
 	'category' => 'plugin',
 	'author' => 'Team NITSAN',
 	'author_email' => 'sanjay@nitsan.in',
