42a53fdc4f163f7391ea0f7500012662417309a7
Khushi Rupera <khushirupera.nitsan@gmail.com>
2025-08-01 16:55:36 +0530

    [TASK]Fix design issue

--- Resources/Public/Js/custom.js
+++ Resources/Public/Js/custom.js
@@ -1,11 +1,14 @@
-$(document).ready(function () {
-	sideHeight();
-});
-$(window).resize(function () {
-	sideHeight();
-});
-
-function sideHeight() {
-	var mapblock = $('.map_canvas').outerHeight();
-	$('.add_sidebar').css('height', mapblock);
-}
+setTimeout(() => {
+	$(document).ready(function () {
+		sideHeight();
+	});
+	$(window).resize(function () {
+		sideHeight();
+	});
+	
+	function sideHeight() {
+		var mapblock = $('.map_canvas').outerHeight();
+		$('.add_sidebar').css('height', mapblock);
+	}
+	
+}, 400);
